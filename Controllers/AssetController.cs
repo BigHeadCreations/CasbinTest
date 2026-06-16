f8f70eb9a4c9eaac322db0dba6ba59df7a676916
Timothy Pearson <timo.555@icloud.com>
2026-06-16 11:48:42 -0700

    Update

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -5,11 +5,12 @@ namespace CasbinTest.Controllers;
 
 [AllowAnonymous]
 [ApiController]
+[Route("assets")]
 public class AssetController(ILogger<AssetController> logger) : ControllerBase
 {
     private readonly ILogger<AssetController> _logger = logger;
 
-    [HttpGet("assets/{id}")]
+    [HttpGet("{id}")]
     public async Task<ActionResult> GetById(int id)
     {
         return Ok(id);
